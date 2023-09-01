c6d004c62d2e40af3864b15ca755af0838fc74b7
lax3331 <la2life1234@gmail.com>
2023-09-01 19:10:29 +0400

    Initial Commit

---
+++ HotelAppSwiftUI/HotelAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelAppSwiftUIApp.swift
+//  HotelAppSwiftUI
+//
+//  Created by Nikita Kuznetsov on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
